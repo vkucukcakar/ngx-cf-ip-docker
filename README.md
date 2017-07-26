7f730841dcbfb296d0bdb5c953c93ace3a36be15
Volkan Kucukcakar <vkucukcakar@users.noreply.github.com>
2017-07-26 21:53:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# vkucukcakar/ngx-cf-ip
+
+ngx-cf-ip as Docker image. (ngx-cf-ip: Cloudflare IP updater for Nginx ngx_http_realip_module)
+
+* Downloads Cloudflare IPv4 and IPv6 lists and merge
+* IP address and list validation just in case
+* Creates a new nginx configuration file using set_real_ip_from directives and IP addresses
+* Configuration file ready to be included (/output/cf.conf)
+* Reloads Nginx without restarting container
+
+## Supported tags
+
+* alpine, latest
+
+## Environment variables supported
+
+* SCHEDULE=["15 3 * * *"]
+	Cron schedule. Do not set to assign default value, daily/03:15.
+* SERVER_CONTAINER_NAME=[server-proxy]
+	Container name of Nginx server to be reloaded. If set, /var/run/docker.sock must be mounted.
+* RELOAD_COMMAND=['echo -e \"POST /containers/server-proxy/kill?signal=HUP HTTP/1.0\r\n\" | nc -U /var/run/docker.sock']
+	Override default reload command. The default reload command sends Nginx container a HUP signal which makes Nginx 
+	reload configuration files without restarting container itself.	There is no need to override default reload 
+	command if you use official Nginx images or any other Nginx image that use Nginx master process as PID 1. 
+	The above example is already the default reload command and just given to demonstrate quoting.
+	In other words, RELOAD_COMMAND should not be set while using official Nginx image or any compatible image.
+* EXTRA_PARAMETERS=['--timeout==60']
+	Extra parameters for ngx-cf-ip, except "-u, --update, -o, --output, -r, --reload" since they are hardcoded and
+	handled by image.
+
+## Example
+
+	$ docker run --name my-cf-ip-updater -v /my/location/cf.conf:/output/cf.conf -v /var/run/docker.sock:/var/run/docker.sock -e SERVER_CONTAINER_NAME=server-proxy -d vkucukcakar/ngx-cf-ip-docker
+
+## Caveats
+
+* Docker socket must me mounted to /var/run/docker.sock for default reload command to work.
+* Output file /output/cf.conf should be mounted and included by your (proxy) Nginx server.
---
+++ files/entrypoint.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+###
+# vkucukcakar/ngx-cf-ip
+# ngx-cf-ip as Docker image. (ngx-cf-ip: Cloudflare IP updater for Nginx ngx_http_realip_module)
+# Copyright (c) 2017 Volkan Kucukcakar
+# 
+# This file is part of vkucukcakar/ngx-cf-ip.
+# 
+# vkucukcakar/ngx-cf-ip is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# vkucukcakar/ngx-cf-ip is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# This copyright notice and license must be retained in all files and derivative works.
+###
+
+
+# Remove previous ngx-cf-ip pid file if survived from an unexpected container crash
+rm /var/run/ngx-cf-ip.pid >/dev/null 2>&1
+
+# Check if the server container to be reloaded is set
+if [ "$SERVER_CONTAINER_NAME" ]; then
+	# Check if docker.sock is mounted
+	if [ ! -e /var/run/docker.sock ]; then
+		echo "Error: /var/run/docker.sock not mounted which is required to reload Nginx when SERVER_CONTAINER_NAME specified."
+		exit 1
+	fi
+	echo "Nginx will be automatically reloaded when Cloudflare IP list updated."
+	# Set reload command
+	if [ "$RELOAD_COMMAND" ]; then
+		_RELOAD="-r --command=\"$RELOAD_COMMAND\" "
+		echo "Custom reload command will be used."
+	else
+		echo "Default reload command will be used."
+		# Sending HUP signal to Nginx container makes Nginx to reload configurations without restarting container (when official or any other Nginx image that using Nginx master process as PID 1)
+		_RELOAD="-r --command=\"echo -e \\\"POST /containers/${SERVER_CONTAINER_NAME}/kill?signal=HUP HTTP/1.0\r\n\\\" | nc -U /var/run/docker.sock\" "
+	fi
+else
+	echo "SERVER_CONTAINER_NAME not set. Nginx will not be automatically reloaded when Cloudflare IP list updated."
+	_RELOAD=""
+fi
+	# Make output directory
+	mkdir /output >/dev/null 2>&1
+	echo "Output directory ready. Output Nginx Cloudflare configuration file will be saved to /output/cf.conf"
+	# Set schedule to daily if empty
+	[ -z "$SCHEDULE" ] && export SCHEDULE="15 3 * * *"
+	# Restore crontab
+	cp /ngx-cf-ip/crontabs/root /etc/crontabs/
+	# Do not echo all with -e to prevent the need for extra quoting text in parameters
+	echo -e "\n" >>/etc/crontabs/root
+	# Add ngx-cf-ip to crontab
+	echo "$SCHEDULE /usr/local/bin/ngx-cf-ip.php -u --output=\"/output/cf.conf\" ${_RELOAD}${EXTRA_PARAMETERS} >>/var/log/cron.log 2>>/var/log/cron-error.log" >>/etc/crontabs/root
+	# Initially run ngx-cf-ip (Note: Without eval, quotes in variable will make command failed. It is related to the behavior of bash and a little complicated...)
+	eval "/usr/local/bin/ngx-cf-ip.php -u --output=\"/output/cf.conf\" ${_RELOAD}${EXTRA_PARAMETERS} >>/var/log/cron.log 2>>/var/log/cron-error.log"
+
+# Execute another entrypoint or CMD if there is one
+if [[ "$@" ]]; then
+	echo "Executing $@"
+	$@
+	EXITCODE=$?
+	if [[ $EXITCODE > 0 ]]; then 
+		echo "Error: $@ finished with exit code: $EXITCODE"
+		exit $EXITCODE; 
+	fi
+fi
